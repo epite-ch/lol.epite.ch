f27d1e441948e961d6cd14174e1d049d62f1d988
Arnaud Rouyer <josh.guthrie@gmail.com>
2011-11-06 06:54:30 +0100

    lol

--- web.js
+++ web.js
@@ -87,7 +87,7 @@ function displayPicture(res, path) {
 }
 
 function itsPicture(path) {
-    return (path.match(/^\/[a-z\%0-9\_\-\!\?\x1C]+\.png$/i));
+    return (path.match(/^\/[a-z\.\%0-9\_\-\!\?]+\.png$/i));
 }
 
 http.createServer(function (req, res) {
